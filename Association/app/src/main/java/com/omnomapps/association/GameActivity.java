adbad9cab0c50cf01cda5569b53897f1a4e5e0af
buchereli <buchereli@gmail.com>
2015-08-07 12:39:50 -0400

    Matched UI

--- Association/app/src/main/java/com/omnomapps/association/GameActivity.java
+++ Association/app/src/main/java/com/omnomapps/association/GameActivity.java
@@ -13,34 +13,76 @@ import java.util.ArrayList;
 public class GameActivity extends Activity {
 
     private ArrayList<Button> left, right;
+    private String selectedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
+
+        selectedButton = "";
 
         left = new ArrayList<>();
         right = new ArrayList<>();
 
-        populate();
+        populateButtons();
     }
 
-    private void populate(){
+    private void populateButtons(){
         left.add((Button)findViewById(R.id.leftButton1));
         left.add((Button)findViewById(R.id.leftButton2));
         left.add((Button)findViewById(R.id.leftButton3));
         left.add((Button)findViewById(R.id.leftButton4));
         left.add((Button)findViewById(R.id.leftButton5));
+        left.add((Button)findViewById(R.id.leftButton6));
+        left.add((Button)findViewById(R.id.leftButton7));
+        left.add((Button)findViewById(R.id.leftButton8));
+        left.add((Button)findViewById(R.id.leftButton9));
+        left.add((Button)findViewById(R.id.leftButton10));
 
-        right.add((Button) findViewById(R.id.rightButton1));
-        right.add((Button) findViewById(R.id.rightButton2));
+        right.add((Button)findViewById(R.id.rightButton1));
+        right.add((Button)findViewById(R.id.rightButton2));
         right.add((Button)findViewById(R.id.rightButton3));
         right.add((Button)findViewById(R.id.rightButton4));
-        right.add((Button) findViewById(R.id.rightButton5));
+        right.add((Button)findViewById(R.id.rightButton5));
+        right.add((Button)findViewById(R.id.rightButton6));
+        right.add((Button)findViewById(R.id.rightButton7));
+        right.add((Button)findViewById(R.id.rightButton8));
+        right.add((Button)findViewById(R.id.rightButton9));
+        right.add((Button)findViewById(R.id.rightButton10));
     }
 
     public void clicked(View v){
         Button button = (Button) v;
-        button.setText("Clicked");
+
+        if(selectedButton.equals("")) {
+            button.setFocusableInTouchMode(true);
+            button.requestFocus();
+
+            selectedButton = button.toString();
+            while(!selectedButton.substring(0, 7).equals("app:id/"))
+                selectedButton = selectedButton.substring(1);
+            selectedButton = selectedButton.substring(7, selectedButton.length()-1);
+            if(selectedButton.startsWith("left"))
+                selectedButton = "L" + selectedButton.substring(10);
+
+            System.out.println(selectedButton);
+        }
+        else {
+            String swap = "";
+
+            if(selectedButton.startsWith("R")) {
+                swap = left.get(Integer.parseInt(selectedButton.substring(1))).getText().toString();
+                left.get(Integer.parseInt(selectedButton.substring(1))).setText(button.getText().toString());
+                left.get(Integer.parseInt(selectedButton.substring(1))).setFocusableInTouchMode(false);
+            }
+            else if(selectedButton.startsWith("L")) {
+                swap = right.get(Integer.parseInt(selectedButton.substring(1))).getText().toString();
+                right.get(Integer.parseInt(selectedButton.substring(1))).setText(button.getText().toString());
+                right.get(Integer.parseInt(selectedButton.substring(1))).setFocusableInTouchMode(false);
+            }
+
+            button.setText(swap);
+        }
     }
 }
